0d40b23ad3770bc3a8c4b1f2f4d09912141514f2
asafzaf <asaf.zafrir@gmail.com>
2023-08-02 12:12:29 +0300

    getting values from outside of the component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {title: "Car Volvo", amount: 37.87, date: new Date(2021, 2, 28)},
+    {title: "Car Toyota", amount: 52.97, date: new Date(2021, 0, 10)},
+    {title: "Car Mazda", amount: 42.99, date: new Date(2021, 6, 14)},
+  ];
+
   return (
     <div>
-      <h1>hi</h1>
-      <ExpenseItem />
+      <h1>This is my main app</h1>
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
+      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
+      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
+
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,13 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+
   return (
     <div className="expense-item">
-      <div>July 24th, 2023</div>
+      <div>{props.date.toISOString()}</div>
       <div className='expense-item__description'>
-        <h2>Car</h2>
-        <div className='expense-item__price'>49.99$</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div>
   );
